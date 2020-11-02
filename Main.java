88b7deb355e625d9197801e6b965322612ea0bae
Girlc7442 <girlc7442@wrdsb.ca>
2020-11-02 14:52:47 +0000

    Finished and discussed in person

--- Main.java
+++ Main.java
@@ -5,7 +5,16 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
+  JPanel mainPanel;
+
+  JButton increaseBtn;
+  JButton resetBtn;
+
+  JLabel increaseLabel;
+  JLabel counterLabel;
+
+  JTextField increaseField;
+  JTextField counterField;
 
 
   // Method to assemble our GUI
@@ -19,7 +28,60 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
+    //initialize the main JPanel
+    mainPanel = new JPanel();
+    //disable any layout helpers
+    mainPanel.setLayout(null);
+
+    //initialize the labels
+    increaseLabel = new JLabel("Increase By:");
+    counterLabel = new JLabel("Counter:");
+
+    //set coordinates and size
+    increaseLabel.setBounds(50,140,150,25);
+    counterLabel.setBounds(50,200,150,25);
+
+    //initialize the text fields
+    increaseField = new JTextField("0");
+    counterField = new JTextField("0");
+
+    //disable the text field so the user can't use it
+    counterField.setEnabled(false);
+
+    //set coordinates and size 
+    increaseField.setBounds(210,130,150,50);
+    counterField.setBounds(210,190,150,50);
+
+    //initialize the buttons
+    increaseBtn = new JButton("Increase");
+    resetBtn = new JButton("Reset");
+
+    //set coordinates and size 
+    increaseBtn.setBounds(50,75,120,25);
+    resetBtn.setBounds(230,75,120,25);
+
+    //add an action listener to the buttons
+    increaseBtn.addActionListener(this);
+    resetBtn.addActionListener(this);
+
+    //set the action command on the buttons
+    increaseBtn.setActionCommand("increase");
+    resetBtn.setActionCommand("reset");
+
+    //add the labels to the mainPanel
+    mainPanel.add(increaseLabel);
+    mainPanel.add(counterLabel);
+
+    //add the text fields to the mainPanel
+    mainPanel.add(increaseField);
+    mainPanel.add(counterField);
+
+    //add the buttons to the mainPanel
+    mainPanel.add(increaseBtn);
+    mainPanel.add(resetBtn);
+
+    //add the panel to the window
+    frame.add(mainPanel);  
 
   }
 
@@ -28,6 +90,32 @@ public class Main implements Runnable, ActionListener{
     // get the command from the action
     String command = e.getActionCommand();
 
+    //initialize an accumulator
+    int counter = 0;
+
+    if(command.equals("increase")){
+      //get the text from each text box
+      String firstText = increaseField.getText();
+      String SecondText = counterField.getText();
+
+      //change the string into an integer
+      int increaseAmount = Integer.parseInt(firstText);
+      int counterStart = Integer.parseInt(SecondText);
+
+      //adds the increaseAmount to the counter
+      counter = counterStart + increaseAmount;
+
+      //print the counter into the counterField
+      counterField.setText("" + counter);
+
+      //prevents the counter from going over 10
+      if(counter >= 10){
+        counterField.setText("10");
+      }
+    }else if(command.equals("reset")){
+      //resets the count to 0
+      counterField.setText("0");
+    }
   }
 
   // Main method to start our program
